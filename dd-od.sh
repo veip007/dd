fe544c6f790c13ca09a539e3a2df71f3899a4a80
veip007 <12257917+veip007@users.noreply.github.com>
2019-09-11 01:39:31 +0800

    傻瓜式一键DD，源备份于OneDrive

---
+++ dd-od.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+MAINIP=$(ip route get 1 | awk '{print $NF;exit}')
+GATEWAYIP=$(ip route | grep default | awk '{print $3}')
+SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
+
+value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
+NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
+
+wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/veip007/dd/master/InstallNET.sh' && chmod a+x InstallNET.sh && wget -N --no-check-certificate https://github.com/veip007/Network-Reinstall-System-Modify/raw/master/Network-Reinstall-System-Modify.sh && chmod a+x Network-Reinstall-System-Modify.sh
+
+clear
+echo "                                                           "
+echo "###########################################################"
+echo "#                                                         #"
+echo "#  Auto DD                                                #"
+echo "#                                                         #"
+echo "#  Last Modified: 2019-09-09                              #"
+echo "#  Linux默认密码：MoeClub.org  or  cxthhhhh.com           #"
+echo "#  Supported by MoeClub                                   #"
+echo "#                                                         #"
+echo "###########################################################"
+echo "                                                           "
+echo "IP: $MAINIP"
+echo "网关: $GATEWAYIP"
+echo "网络掩码: $NETMASK"
+echo ""
+echo "请选择您需要的镜像包:"
+echo "  1) CentOS 7 (DD) 用户名：root 密码：Pwd@CentOS"
+echo "  2) CentOS 6 (阿里云镜像) 用户名：root 密码：MoeClub.org"
+echo "  3) CentOS 6 用户名：root 密码：MoeClub.org"
+echo "  4) Debian 7 x32 用户名：root 密码：MoeClub.org"
+echo "  5) Debian 8 x64 用户名：root 密码：MoeClub.org"
+echo "  6) Debian 9 x64 用户名：root 密码：MoeClub.org"
+echo "  7) Debian 10 x64 用户名：root 密码：cxthhhhh.com"
+echo "  8) Ubuntu 14.04x64 用户名：root 密码：MoeClub.org"
+echo "  9) Ubuntu 16.04x64 用户名：root 密码：MoeClub.org"
+echo "  10) Ubuntu 18.04x64 用户名：root 密码：MoeClub.org"
+echo "  11) 萌咖Win7x64 用户名:Administrator  密码：Vicer"
+echo "  12) Win2019 By:MeowLove  密码：cxthhhhh.com"
+echo "  13) Win2016 By:MeowLove  密码：cxthhhhh.com"
+echo "  14) Win2012 R2 By:MeowLove  密码：cxthhhhh.com"
+echo "  15) Win2008 R2 By:MeowLove  密码：cxthhhhh.com"
+echo "  16) Windows 7 Vienna By:MeowLove  密码：cxthhhhh.com"
+echo "  17) Windows 2003 Vienna By:MeowLove  密码：cxthhhhh.com"
+echo "  18) Win7x32 By:不知道作者了  用户名:Administrator  密码：Windows7x86-Chinese"
+echo "  19) Win-2003x32 By:老司机  用户名:Administrator  密码：WinSrv2003x86-Chinese"
+echo "  20) Win2008x64 By:老司机  用户名:Administrator  密码：WinSrv2008x64-Chinese"
+echo "  21) Win2012R2x64 By:老司机  用户名:Administrator  密码：WinSrv2012r2"
+echo "  自定义安装请使用：bash InstallNET.sh -dd '您的直连'"
+echo ""
+echo -n "请输入编号: "
+read N
+case $N in
+  1) echo "Password: Pwd@CentOS" ; read -s -n1 -p "Press any key to continue..." ; bash InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd 'https://api.moetools.net/get/centos-7-image' ;;
+  2) bash InstallNET.sh -c 6.9 -v 64 -a --mirror 'http://mirrors.aliyun.com/centos-vault' --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $SUBNET ;;
+  3) bash InstallNET.sh -c 6.9 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  4) bash InstallNET.sh -d 7 -v 32 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  5) bash InstallNET.sh -d 8 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  6) bash InstallNET.sh -d 9 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  7) bash Network-Reinstall-System-Modify.sh -Debian_10 ;;
+  8) bash InstallNET.sh -u trusty -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  9) bash InstallNET.sh -u xenial -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  10) bash InstallNET.sh -u bionic -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
+  11) bash InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd 'https://veip-my.sharepoint.com/:u:/g/personal/admin_veip_onmicrosoft_com/EUXeAeHaWcxHoA34etRizncB8gCA-hOloXzFGcGpLPPuqg?download=1';;
+  12) bash Network-Reinstall-System-Modify.sh -Windows_Server_2019 ;;
+  13) bash Network-Reinstall-System-Modify.sh -Windows_Server_2016 ;;
+  14) bash Network-Reinstall-System-Modify.sh -Windows_Server_2012R2 ;;
+  15) bash Network-Reinstall-System-Modify.sh -Windows_Server_2008R2 ;;
+  16) bash Network-Reinstall-System-Modify.sh -Windows_7_Vienna ;;
+  17) bash Network-Reinstall-System-Modify.sh -Windows_Server_2003 ;;
+  18) bash InstallNET.sh -dd 'https://veip-my.sharepoint.com/:u:/g/personal/admin_veip_onmicrosoft_com/EbTzZAGGydxLizEnpUHuOxMB5xQXeobqGZu0K-AeMRz3vA?download=1' ;;
+  19) bash InstallNET.sh -dd 'https://veip-my.sharepoint.com/:u:/g/personal/admin_veip_onmicrosoft_com/ESGlMpUoGRBLqIceOVLllWIB67Hg-H-dRtidPLjZiHwG-g?download=1' ;;
+  20) bash InstallNET.sh -dd 'https://veip-my.sharepoint.com/:u:/g/personal/admin_veip_onmicrosoft_com/EQnjm0D1oA9GiW2Kp2DnYuIBBqLhV-AxR3Y8cAPkkhC3mw?e=4YG80Q' ;;
+  21) bash InstallNET.sh -dd 'https://veip-my.sharepoint.com/:u:/g/personal/admin_veip_onmicrosoft_com/ES0CItHE0oFEnseoBdygVrgBF6cJOw_Jm7PV_vPLWcdaNQ?e=5DEPSh' ;;
+  *) echo "Wrong input!" ;;
+esac
